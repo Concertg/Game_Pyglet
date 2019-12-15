88ef7eed162c53d479b866598c05b2f1b7d39b05
Concertg <sasha.noskov.2227@mail.ru>
2019-12-15 11:48:46 +1000

    -

---
+++ main.py
@@ -0,0 +1,41 @@
+import pyglet
+from pyglet import gl
+from pyglet.window import key
+from pyglet.gl import *
+import math
+
+W, H = 780, 630
+BG_COLOR = (0.0, 0.0, 0.0, 1.)
+
+RADIUS = 60
+x = W // 2
+y = H // 2
+
+window = pyglet.window.Window(width=W, height=H, caption='Game')
+window.set_location(5, 30)
+window.set_mouse_visible(visible=False)
+counter = pyglet.window.FPSDisplay(window=window)
+
+
+def update(dt):
+    pass
+
+
+@window.event
+def on_draw():
+    window.clear()
+    glLoadIdentity()
+    glColor4f(1., 0., 0., 0.75)
+    glBegin(GL_TRIANGLE_FAN)
+    for angle in range(0, 360, 10):
+        rads = math.radians(angle)
+        s = RADIUS * math.sin(rads)
+        c = RADIUS * math.cos(rads)
+        glVertex3f(x + c, y + s, 0)
+    glEnd()
+    counter.draw()
+
+
+gl.glClearColor(*BG_COLOR)
+pyglet.clock.schedule_interval(update, 1 / 60.0)
+pyglet.app.run()
